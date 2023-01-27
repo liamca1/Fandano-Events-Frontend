cec2109adbec6f5454b02f9bc559b25ff7d66120
Liam Carroll <liamcarroll@Laptop.local>
2023-01-27 17:17:07 +0100

    change to js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import styles from "./index.module.css";
 import { Fragment } from "react";
 import { fetcher } from "../lib/api";
 
@@ -15,11 +13,10 @@ function HomePage({ posts }) {
 
 export default HomePage;
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const postsResponse = await fetcher(
     `${process.env.NEXT_PUBLIC_STRAPI_URL}/posts`
   );
-  console.log(postsResponse);
   return {
     props: {
       posts: postsResponse.data,
